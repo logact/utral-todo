6144178257446fc30d1114c765465549ed17f6f0
logact <logact@qq.com>
2026-06-18 19:23:43 +0800

    fix docker compose build failed

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -10,7 +10,7 @@ if (!globalThis.crypto?.randomUUID) {
       const r = (Math.random() * 16) | 0;
       const v = c === 'x' ? r : (r & 0x3) | 0x8;
       return v.toString(16);
-    });
+    }) as `${string}-${string}-${string}-${string}-${string}`;
   };
 }
 
