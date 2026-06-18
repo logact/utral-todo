4454069246a3bbceeb89b65f83b2ce6ff7d0187c
logact <logact@qq.com>
2026-06-18 08:48:23 +0800

    adjust the size of sync size to 10mb

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -42,7 +42,7 @@ function requireAuth(req: express.Request, res: express.Response, next: express.
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api', requireAuth);
 
